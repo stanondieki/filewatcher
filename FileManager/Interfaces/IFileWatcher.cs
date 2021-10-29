532aba70e814490ccae7e711b4aea07602aa94cb
stanondieki <89203682+stanondieki@users.noreply.github.com>
2021-10-29 17:55:03 +0300

    Revert "Sample project"

--- FileManager/Interfaces/IFileWatcher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileManager.Interfaces
-{
-    public interface IFileWatcher
-    {
-        // Do not make any changes to this file
-        string ReadFile(string path);
-        void WriteFile(string path, string content);
-    }
-}
