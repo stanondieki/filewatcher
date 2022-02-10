8a81efe243cbd53b5ff8e531e3f7e49f8c7881f3
Nyandika Donald <ondiekistanley21@gmail.com>
2022-02-10 14:03:49 +0300

    correction commit

--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -5,15 +5,49 @@ using System.IO;
 
 namespace FileManager
 {
+    public interface IFileWatcher
+    {
+        void ReadFile(string path);
+        void WriteFile(string path, string content);
+    }
+    public class FileWatcher : IFileWatcher
+    {
+        public void ReadFile(string path)
+        {
+            if(File.Exists(path))
+            {
+                string [] lines=File.ReadAllLines(path);
+
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("file does not exist");
+            }
+
+        }
+        public void WriteFile(string path, string content)
+        {
+            string[] lines = { "care, tear, gear" };
+
+            File.WriteAllLines(path, lines);
+            File.AppendAllLines(path, lines);
+
+        }
+    }
     internal class Program
     {
             static void Main(string[] args)
             {
             string path = @"D:\try\sample.txt";
 
-            FileWatcher fileWatcher = new FileWatcher();
-            Console.WriteLine();
+            FileWatcher watcher = new FileWatcher();
 
+            watcher.ReadFile(path);
+           
                 // Use the interface to get access to the methods
                 // Create a sample.txt file anywhere in your disk 
                 // Read the file using the ReadFile method (If the file does not exist in the given path pass the message accodingly)
